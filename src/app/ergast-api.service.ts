29efb01f20f4e853f9dc4929d7202171e1048d26
Alexandros Kalamatianos <alex.kalamatianos@gmail.com>
2018-04-20 01:42:08 +0300

    Http changed to Https

--- src/app/ergast-api.service.ts
+++ src/app/ergast-api.service.ts
@@ -7,12 +7,12 @@ export class ErgastApiService {
   constructor(private http: HttpClient) { }
 
   getDriversList(){
-    const url="http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK"
+    const url="https://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK"
     return this.http.jsonp(url,'callback');
   }
 
   getDriverDetails(entityId){
-    const url=`http://ergast.com/api/f1/2013/drivers/${entityId}/driverStandings.json?callback=JSON_CALLBACK`;
+    const url=`https://ergast.com/api/f1/2013/drivers/${entityId}/driverStandings.json?callback=JSON_CALLBACK`;
     return this.http.jsonp(url,'callback');
   }
 
